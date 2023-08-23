529c2ca06452f8eaf1a5238beb599afd1bbf2ab7
Jacob Alheid <shakefu@gmail.com>
2023-08-23 15:53:40 -0700

    fix: single hook, working debug

--- debug.sh
+++ debug.sh
@@ -8,9 +8,16 @@ function gha_debug {
     echo
     echo "gha_debug"
     echo
+    echo "Script: $0"
+    echo
     echo "Args: $*"
     echo
     echo "Environment:"
+    for name in $(declare -F | awk '{print $3}' | grep 'gha_'); do
+        unset "$name"
+    done
     env | sort
+    # env | sort
+    # env | awk 'sub(/}/,"}\n")||$1=$1' ORS=''
 }
 export -f gha_debug
--- hook.sh
+++ hook.sh
@@ -4,7 +4,8 @@ echo
 echo "start.sh"
 echo
 
-DEBUG_SCRIPT="https://raw.githubusercontent.com/shakefu/gha-debug/main/debug.sh"
+DEBUG_SHA="main"
+DEBUG_SCRIPT="https://raw.githubusercontent.com/shakefu/gha-debug/$DEBUG_SHA/debug.sh"
 
 # shellcheck disable=SC1090
 source <(wget -qO- "$DEBUG_SCRIPT")
