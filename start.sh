1e7a0c740b471fcc484f242c9355add8b38a6f0d
Jacob Alheid <shakefu@gmail.com>
2023-08-23 15:20:19 -0700

    fix: huh

--- start.sh
+++ start.sh
@@ -7,16 +7,25 @@ echo
 # shellcheck disable=SC1090
 source <(wget -qO- https://raw.githubusercontent.com/shakefu/gha-debug/main/debug.sh)
 
-echo "Environment:"
-env | sort
-echo
+# echo "Environment:"
+# env | sort
+# echo
+main() {
+    echo
+    echo "main"
+    echo
 
-echo "Functions:"
-declare -F
-echo
+    echo "Functions:"
+    declare -F
+    echo
 
-echo "Functions with -F:"
-typeset -F
-echo
+    echo "Functions with -F:"
+    typeset -F
+    echo
 
-gha_debug "$@"
+    wget -qO- https://raw.githubusercontent.com/shakefu/gha-debug/main/debug.sh
+
+    gha_debug "$@"
+}
+
+main "$@"
